c0c6bcb9eb8c3939f6f2028e31b4ccafa3087a0b
Yuchen Wang <yuchenw2015@gmail.com>
2019-03-12 22:09:05 -0500

    set up the data and the basic stuff.

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Shape file of Australia.
+#'
+#' A dataset containing the shape details of Australia.
+#'
+#' @format A data list with 10 vectors and 1 list, we would focus on the list "geometry":
+#' \describe{
+#'   \item{geometry}{A list (deepth = 3) contains the latitudes and longitudes.}
+#' }
+#' @source \url{https://gadm.org/download_country_v3.html}
+#' @docType data
+#' @keywords datasets
+#' @name AUS
+#' @usage read_sf("data/gadm36_ALB_shp/gadm36_ALB_1.shp")
+NULL
