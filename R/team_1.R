8c9b46c6e9a64b27c65e6c5ec6e6c482742e7af1
Yuchen Wang <yuchenw2015@gmail.com>
2019-03-14 10:18:06 -0500

    update

--- R/team_1.R
+++ R/team_1.R
@@ -1,12 +1,36 @@
+#' Plot a country shape with .shp file.
+#'
+#' @param file The name of the file which the data are to be read from. The file should be in shp format.
+#' @param tolerance The tolerance level to be used in thinning the origninal data. The default value is 0.1. The original data usually is too large to run. Larger tolerance value makes the data thinner.
+#' @export
+#' @return A list includes a vector of country name, a vector of the states name, and a data frame of the polygon longitudes and latitudes.
+#' @examples
+#' #See details about this example data with ?gadm36_AUS_1.shp
+#' fp <- "./data/gadm36_AUS_shp/gadm36_AUS_1.shp"
+#' team_1(fp)
+#' team_1(fp, 0.3)
+
 team_1 <- function(file, tolerance = 0.1){
+  file_format <- substr(file, nchar(file) - 2, nchar(file))
+  assertthat::assert_that(file_format == "shp", msg = "The file is not .shp format!")
   ozbig <- sf::read_sf(file)
   oz_st <- maptools::thinnedSpatialPoly(as(ozbig, "Spatial"), tolerance = tolerance, minarea = 0.001, topologyPreserve = TRUE)
   oz <- sf::st_as_sf(oz_st)
+  assertthat::assert_that(!is.null(oz), msg = "Fail to thinner the data!")
+  assertthat::assert_that(!is.null(oz$NAME_0), msg = "The data does not include column 'NAME_0' which is the name of the country!")
+  assertthat::assert_that(!is.null(oz$NAME_1), msg = "The data does not include column 'NAME_1' which is the name of the states!")
+  assertthat::assert_that(is.list(oz$geometry), msg = "geometry is not a list!" )
+  assertthat::assert_that(is.matrix(oz$geometry[[1]][[1]][[1]]), msg = "geometry is not a list (with depth = 3) of matrices!" )
   df.oz.purr <- oz$geometry %>%
     purrr::map_depth(3, data.frame) %>%
     purrr::flatten() %>%
     purrr::flatten() %>%
     dplyr::bind_rows(.id = "group") %>%
-    rename("lat" = y, "long" = x)
-  list(country = oz$NAME_0[1], polygon = df.oz.purr)
+    dplyr::rename("lat" = y, "long" = x)
+  assertthat::assert_that(!is.null(df.oz.purr), msg = "Fail to flatten list!")
+  assertthat::assert_that(is.data.frame(df.oz.purr), msg = "Fail to generate the data frame!")
+  assertthat::assert_that(!is.null(df.oz.purr$group), msg = "Fail to generate the column 'group'!")
+  assertthat::assert_that(!is.null(df.oz.purr$lat), msg = "Fail to generate the column 'lat' (latitude)!")
+  assertthat::assert_that(!is.null(df.oz.purr$lat), msg = "Fail to generate the column 'long' (longitude)!")
+  list(country = oz$NAME_0[1], state = oz$NAME_1, polygon = df.oz.purr)
 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Team6)
+
+test_check("Team6")
