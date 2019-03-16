61daf04fc04489fffdea23a991dc0fc3b1c4bf49
Yuchen Wang <yuchenw2015@gmail.com>
2019-03-16 16:15:39 -0500

    combine the functions for "team_8"

--- R/helper.R
+++
@@ -1,7 +0,0 @@
-helper <- function(y) {
-  data <- data.frame(long = y[, 1],
-                     lat  = y[, 2],
-                     groups = rep(rnorm(1),nrow(y)),
-                     order = 1:nrow(y))
-  data
-}
